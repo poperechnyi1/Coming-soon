4859e5534e562ba528cc072311d8f490cce69cdb
Stepan <poperechnyi1@gmail.com>
2016-09-05 22:08:27 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+            //set width of wrapper;
+          $('#no-reflection').county({ endDateTime: new Date('2016/12/31 23:59'), reflection: false, animation: 'fade', theme: 'blue' });
+        });
